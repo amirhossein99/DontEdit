d3c1f5f2f9fe331095761a6f2246d6625b3afbd4
amirhossein99 <amousavi316@gmail.com>
2016-12-17 15:23:44 +0330

    Add files via upload

---
+++ DontEdit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while true; do
+  echo -e "$bld$f4 CHECKING PROCESSES...$rst"
+	echo "لطفا چند ثانیه صبر کنید"
+  echo "ران کردن بات شروع شد"
+  echo "موفقیت در ران کردن بات"
+  echo "خیلی خوبه"
+  python DontEdit.py
+	echo "مشکلی در ران کردن بات❌، لطفا بعدا تلاش کنید"
+	sleep 3s
+done
